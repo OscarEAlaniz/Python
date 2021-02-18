c295543b015734652ff300e815747fc09dcc901f
OscarEAlaniz <oscar.esteban.alaniz@gmail.com>
2021-02-17 21:08:03 -0800

    update

---
+++ 01-Holamundo/comentarios.py
@@ -0,0 +1,12 @@
+#esto no se mostrá el texto por consola 
+print("hola")
+print("mundo")
+print("!!!")
+
+"""
+eso no se mostrá 
+print("mundo")
+
+print("!!!")
+"""
+print("adios")
---
+++ 01-Holamundo/holamundo.py
@@ -0,0 +1,1 @@
+print("hola mundo soy el ingeniero oscar alaniz")
