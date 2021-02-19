d60214c7a87964d21122a3a292a26d38f141f65a
OscarEAlaniz <oscar.esteban.alaniz@gmail.com>
2021-02-18 18:10:30 -0800

    uptdate

---
+++ 09-listas/crearListas.py
@@ -0,0 +1,26 @@
+"""
+Listas (arrays)
+Son colecciones o conjunotos de datos/valores, bajo
+un unico nombre.
+Para acceder a estos valores podemos usar un idice numerico
+
+"""
+# definiciones de lista
+
+pelicula ="Batman"
+peliculas =[ "batman ", "spiderman", "el seño de los anillos"]
+cantantes = list(('2pac','drake'))
+years =list(range(2020,2250))
+variada = ["oscar",30, True, "texto"]
+
+print(peliculas)
+print(cantantes)
+print(years)
+print(variada)
+
+
+
+
+
+
+
