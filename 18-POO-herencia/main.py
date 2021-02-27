053352ccce16b07e9f70fcafe058c5f9f66cc869
OscarEAlaniz <oscar.esteban.alaniz@gmail.com>
2021-02-26 22:11:56 -0800

    update

--- 18-POO-herencia/main.py
+++ 18-POO-herencia/main.py
@@ -10,3 +10,17 @@ persona.setEdad("1000 años")
 
 
 print(f"la persona es: {persona.getNombre()} {persona.getApellidos()}")
+
+
+informatico = clases.Informatico()
+
+informatico.setNombre("Carlos")
+informatico.setApellidos("Martinez")
+
+print ("---Herencia----")
+print(f"El informaticos es {informatico.getNombre()} {informatico.getApellidos()}")
+print(informatico.getLenguajes())
+
+
+tecnico = clases.TecnicoRedes()
+print(tecnico.auditarRedes)
